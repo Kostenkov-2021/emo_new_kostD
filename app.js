cc12430494ddc1ac6d277d171b82c8251a9156a9
Zoya <romanova.zoya.2002@mail.ru>
2021-09-02 14:10:06 +0300

    logs

--- app.js
+++ app.js
@@ -61,10 +61,10 @@ mongoose.connect(keys.mongoURI, {
     .then(() => console.log('MongoDB connected'))
     .catch(error => console.log(error))
 
+app.use(require('morgan')('dev'))
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
 
-app.use(require('morgan')('dev'))
 app.use('/uploads', express.static('uploads'))
 app.use('/images', express.static('images'))
 app.use(bodyParser.urlencoded({extended: true}))
