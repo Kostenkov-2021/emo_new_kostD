4236bfe706909dfd0742334005126279f51c1447
ZRomanova <romanova.zoya.2002@mail.ru>
2022-03-09 17:56:55 +0100

    commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const http = require('http').createServer(app)
 
 
 if (process.env.NODE_ENV === 'production') {
-  var io = require('socket.io')(3001, {
+  var io = require('socket.io')(http, {
     cors: {
       origin: '*'
     }
