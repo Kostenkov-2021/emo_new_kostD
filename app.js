a57c0377df2d867d578de4ce67aa45f39c2de474
Zoya <romanova.zoya.2002@mail.ru>
2022-03-09 15:12:39 +0300

    Message

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const keys = require('./config/keys')
 
 const app = express()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http, {origins: ["http://localhost:4200", "https://emo.su"]})
+const io = require('socket.io')(http, {origins: ["http://localhost:4200", "https://emo.su:443"]})
 
 const {ExpressPeerServer} = require('peer')
 
