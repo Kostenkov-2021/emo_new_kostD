51cf156b168846bae8a3ba89f823eef82bea0d0e
Zoya <romanova.zoya.2002@mail.ru>
2022-03-11 14:27:35 +0300

    Video twice block

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: true,
-  SOCKET_ENDPOINT: `emo.su:444`,
+  SOCKET_ENDPOINT: `socket.emo.su`,
   isMobile: window.innerWidth < 700,
   host: 'emo.su',
-  port: 444,
+  port: 443,
   url: `https://emo.su`
 };
