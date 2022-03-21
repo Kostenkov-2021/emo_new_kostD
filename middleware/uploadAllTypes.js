e0e6176b262a7ed05889a613b884db276921e855
Zoya <romanova.zoya.2002@mail.ru>
2022-03-21 21:46:50 +0300

    Message

--- middleware/uploadAllTypes.js
+++ middleware/uploadAllTypes.js
@@ -1,5 +1,6 @@
 const multer = require('multer')
 const moment = require('moment')
+const path = require('path');
 const cyrillicToTranslit = require('cyrillic-to-translit-js')
 
 function getRandomInt(min, max) {
@@ -8,7 +9,7 @@ function getRandomInt(min, max) {
 
 const storage = multer.diskStorage({
   destination(req, file,  cb) {
-    cb(null, '../uploads/')
+    cb(null, path.join(__dirname, '../uploads/'));
   },
   filename(req, file, cb) {
     cb(null, `${moment().format('DDMMYYYY-HHmmss_SSS')}-${getRandomInt(1, 10000)}-${getRandomInt(1, 10000)}-emoall-${cyrillicToTranslit().transform(file.originalname, "_")}`)
